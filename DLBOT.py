40b72d50a5c5ba7fe7e1dd82eafeabf52e8530e3
ks-russell <sobaman14@gmail.com>
2015-11-10 18:40:52 -0800

    Delete DLBOT.py

--- DLBOT.py
+++
@@ -1,9 +0,0 @@
-import praw
-user_agent = ("Duelyst_Cards 0.1")
-r = praw.Reddit(user_agent = user_agent)
-subreddit = r.get_subreddit("duelyst")
-for submission in subreddit.get_hot(limit = 5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("---------------------------------\n")
